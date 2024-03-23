6b09037c52f0f83770a29a14ad3f6d62c6e45b66
rodstone <rodtronicsnz@gmail.com>
2024-03-23 16:44:54 +1300

    title and start of gizmo
    adding functionality of the logo cycling colors when clicked on
    
    and made start toward gizmo making

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let ccCodeName = "deep alpha";
 
 
 
-
+// this part of the script manages the changing of tabs when clicked upon
 // load the references to the tabs into this array
 var tabElement = [];
 tabElement[0] = document.getElementById("tabA_ID");
@@ -17,33 +17,53 @@ tabElement[1] = document.getElementById("tabB_ID");
 tabElement[2] = document.getElementById("tabC_ID");
 var tabTotalNumber = tabElement.length;
 
-
-
-
-
 // this is the code for switching between tabs
 // it doesn't affect the page. just the tabs
-
 function setActiveTab(tabNumber) {
-
-    //make sure trying to set legit tab value
-    // unecessary imo
-    // if ((tabNumber < 0) || (tabElement > (tabTotalNumber - 1))) {
-    //     console.log("OOB trying to set active tab");
-    //     return;
-    // }
-
     // clear all tabs to inactive
     for (let index = 0; index < tabTotalNumber; index++) {
         tabElement[index].setAttribute("data-tabState", "inactive");
     }
-
     // now set which one active
     tabElement[tabNumber].setAttribute("data-tabState", "active");
-
 }
-
 // set up event listeners for tabs
 for (let index = 0; index < tabTotalNumber; index++) {
     tabElement[index].addEventListener("click", () => setActiveTab(index));
+}
+// done
+
+
+
+// this part of the script cycles the logo colors when its clicked on
+// variables related to cycling the logo colors
+var logoColorIndex = 0;
+var logoColorLength = 6;
+var logoElement = document.getElementById("titleID");
+// a series of colors to cycle through
+var logoColorArray = ["#CA2C92", "#008080", "#FF7F11", "#50C878", "#EE4B2B", "#01baef", ];
+// set up event listener for clicking on logo
+logoElement.addEventListener("click", () => cycleLogoColor());
+// function that cycles logo colors;
+function cycleLogoColor() {
+    logoColorIndex++;
+    logoColorIndex = logoColorIndex % logoColorLength
+    logoElement.style.color = logoColorArray[logoColorIndex];
+}
+// done
+
+
+
+// this part of the script is to try and create a gizmo
+// this just appends one of these into the crimeGizmoWrapper
+var crimeGizmoWrapperElement = document.getElementById("crimeGizmoWrapper_ID");
+
+function createGizmoTest(testWords) {
+    let newGizmo = document.createElement("div");
+    newGizmo.classList.add("gizmo");
+    newGizmo.innerHTML = testWords;
+    crimeGizmoWrapperElement.appendChild(newGizmo);
+}
+for (let index = 0; index < 16; index++) {
+    createGizmoTest("newrods " + index);
 }
